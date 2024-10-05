e9895560d524db37a3b0abf130ecc9868b956a10
KushSatani <kushsatani@gmail.com>
2024-10-05 10:47:04 -0500

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,25 +1,16 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+// Hamburger menu toggle functionality
+const menuIcon = document.querySelector('.menuIcon');
+const closeIcon = document.querySelector('.closeIcon');
+const menu = document.querySelector('.menu');
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
+menuIcon.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+});
 
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenu);
-  }
-)
+closeIcon.addEventListener('click', () => {
+    menu.style.display = 'none';
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+});
